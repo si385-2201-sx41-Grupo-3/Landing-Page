80950f15df08160abe8029099d4fced73604cd62
adrianesqueiros <u201718169@upc.edu.pe>
2022-07-01 00:39:53 -0500

    feature language

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -111,5 +111,23 @@ Author: GrayGrids
         navbarToggler.classList.toggle("active");
     });
 
+    var check=document.querySelector(".check");
+check.addEventListener('click',idioma);
 
+function idioma(){
+    let id=check.checked;
+    if(id==true){
+        location.href="indexES.html";
+        document.getElementsByClassName('es').style = "color: #00a1ff";
+        document.getElementsByClassName('en').style= "color: #333";
+    }
+    else{
+        location.href="index.html"
+        document.getElementsByClassName('en').style = "color: #00a1ff";
+        document.getElementsByClassName('es').style= "color: #333";
+        
+    }
+}
+
+    
 })();
